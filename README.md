761ee2cd0bb25bc63bd9d0c9d164695289c11c03
wl500g <wlcims@163.com>
2020-09-06 00:49:49 +0800

    fix upload.sh

--- README.md
+++ README.md
@@ -28,6 +28,12 @@ pip install esptool
 
 esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x1000 bootloader_dio_80m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 ttgo-t-watch2020_v1.ino.bin
 ```
+> Put all files in the same folder   
+> bootloader_dio_80m.bin  
+> partitions.bin  
+> boot_app0.bin  
+> ttgo-t-watch2020_v1.ino.bin  
+> upload.sh
 
 ## 3. Windows
 
--- upload.sh
+++ upload.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x1000 bootloader_dio_80m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 firmware.bin
+esptool.py --chip esp32 --port "/dev/ttyUSB0" --baud 2000000 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect 0x1000 bootloader_dio_80m.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 ttgo-t-watch2020_v1.ino.bin
+
+
